1ccb1321a66af4fdf3f6ec71b6af1922340d0d80
Qin Yu <qin.yu.95@outlook.com>
2022-05-04 16:04:09 +0200

    Create MultiheadHDF5Dataset class draft

--- pytorch3dunet/datasets/hdf5.py
+++ pytorch3dunet/datasets/hdf5.py
@@ -208,6 +208,115 @@ class AbstractHDF5Dataset(ConfigDataset):
         return results
 
 
+class MultiheadHDF5Dataset(AbstractHDF5Dataset):
+    def __init__(self, file_path,
+                 phase,
+                 slice_builder_config,
+                 transformer_config,
+                 mirror_padding=(16, 32, 32),
+                 raw_internal_path='raw',
+                 label_internal_path='label',
+                 weight_internal_path=None,
+                 global_normalization=True):
+        """
+        :param file_path: path to H5 file containing raw data as well as labels and per pixel weights (optional)
+        :param phase: 'train' for training, 'val' for validation, 'test' for testing; data augmentation is performed
+            only during the 'train' phase
+        :para'/home/adrian/workspace/ilastik-datasets/VolkerDeconv/train'm slice_builder_config: configuration of the SliceBuilder
+        :param transformer_config: data augmentation configuration
+        :param mirror_padding (int or tuple): number of voxels padded to each axis
+        :param raw_internal_path (str or list): H5 internal path to the raw dataset      # List is not implemented
+        :param label_internal_path (str or list): H5 internal path to the label dataset  # List is not implemented
+        :param weight_internal_path (str or list): H5 internal path to the per pixel weights
+        """
+        assert phase in ['train', 'val', 'test']
+        if phase in ['train', 'val']:
+            mirror_padding = None
+
+        if mirror_padding is not None:
+            if isinstance(mirror_padding, int):
+                mirror_padding = (mirror_padding,) * 3
+            else:
+                assert len(mirror_padding) == 3, f"Invalid mirror_padding: {mirror_padding}"
+
+        self.mirror_padding = mirror_padding
+        self.phase = phase
+        self.file_path = file_path
+
+        input_file = self.create_h5_file(file_path)
+
+        self.raw = self.fetch_and_check(input_file, raw_internal_path)
+
+        if global_normalization:
+            stats = calculate_stats(self.raw)
+        else:
+            stats = {'pmin': None, 'pmax': None, 'mean': None, 'std': None}
+
+        self.transformer = transforms.Transformer(transformer_config, stats)
+        self.raw_transform = self.transformer.raw_transform()
+
+        if phase != 'test':
+            # create label/weight transform only in train/val phase
+            self.label_transform = self.transformer.label_transform()
+            self.label = self.fetch_and_check(input_file, label_internal_path)
+
+            if weight_internal_path is not None:
+                # look for the weight map in the raw file
+                self.weight_map = self.fetch_and_check(input_file, weight_internal_path)
+                self.weight_transform = self.transformer.weight_transform()
+            else:
+                self.weight_map = None
+
+            self._check_volume_sizes(self.raw, self.label)
+        else:
+            # 'test' phase used only for predictions so ignore the label dataset
+            self.label = None
+            self.weight_map = None
+
+            # add mirror padding if needed
+            if self.mirror_padding is not None:
+                z, y, x = self.mirror_padding
+                pad_width = ((z, z), (y, y), (x, x))
+                if self.raw.ndim == 4:
+                    channels = [np.pad(r, pad_width=pad_width, mode='reflect') for r in self.raw]
+                    self.raw = np.stack(channels)
+                else:
+                    self.raw = np.pad(self.raw, pad_width=pad_width, mode='reflect')
+
+        # build slice indices for raw and label data sets
+        slice_builder = get_slice_builder(self.raw, self.label, self.weight_map, slice_builder_config)
+        self.raw_slices = slice_builder.raw_slices
+        self.label_slices = slice_builder.label_slices
+        self.weight_slices = slice_builder.weight_slices
+
+        self.patch_count = len(self.raw_slices)
+        logger.info(f'Number of patches: {self.patch_count}')
+
+    def __getitem__(self, idx):
+        if idx >= len(self):
+            raise StopIteration
+
+        # get the slice for a given index 'idx'
+        raw_idx = self.raw_slices[idx]
+        # get the raw data patch for a given slice
+        raw_patch_transformed = self.raw_transform(self.raw[raw_idx])
+
+        if self.phase == 'test':
+            # discard the channel dimension in the slices: predictor requires only the spatial dimensions of the volume
+            if len(raw_idx) == 4:
+                raw_idx = raw_idx[1:]
+            return raw_patch_transformed, raw_idx
+        else:
+            # get the slice for a given index 'idx'
+            label_idx = self.label_slices[idx]
+            label_patch_transformed = self.label_transform(self.label[label_idx])
+            if self.weight_map is not None:
+                weight_idx = self.weight_slices[idx]
+                weight_patch_transformed = self.weight_transform(self.weight_map[weight_idx])
+                return raw_patch_transformed, label_patch_transformed, weight_patch_transformed
+            # return the transformed raw and label patches
+            return raw_patch_transformed, label_patch_transformed
+
 class StandardHDF5Dataset(AbstractHDF5Dataset):
     """
     Implementation of the HDF5 dataset which loads the data from all of the H5 files into the memory.
